580905bb425790c30a6dd7325ee60c25911f378e
cardosdan <cardosdan08@gmail.com>
2021-12-31 15:07:32 +0200

    pagina de home,store, ii aproape gata,

---
+++ src/actions/contact_us_database.php
@@ -0,0 +1,23 @@
+<?php
+include("../includes/db.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $nume = $_POST["nume"];
+    $email = $_POST["email"];
+    $subiect = $_POST["subiect"];
+//    session_start();
+//    $user_id = $_SESSION['id'];
+    $sql = "INSERT INTO contact_us (nume,email,subiect) VALUES ('{$nume}','{$email}','{$subiect}')";
+    if ($con->query($sql) === TRUE) {
+        echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $con->error;
+    }
+
+    $con->close();
+
+    header("location: ../../contact_us.html");
+
+}
